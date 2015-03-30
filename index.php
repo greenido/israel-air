b9fcfb532e46e0c98b3a919df1ef0a8a9784f302
Ido Green <greenido@gmail.com>
2015-03-30 14:02:29 +0300

    New map with nice icons

--- index.php
+++ index.php
@@ -93,10 +93,16 @@
 
       <div class="span5" dir="rtl">
         <h2>Map</h2>
-          <iframe id="ifrMap" height="650" width="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="מפת המפעלים המדווחים למפלס ממוינים לפי ענפי פעילות" src="http://beta.govmap.gov.il/map.aspx?c=eF08XGXO0ta3TQtFnHkc8w%3d%3d%7C5yAAYB898T4%2b6C1wazlNPA%3d%3d&amp;showBackBtn=1&amp;showNavBtn=1&amp;AllowDrag=1&amp;in=1&amp;height=650&amp;width=500&amp;z=0.5&amp;b=0&amp;mapMode=1&amp;lay=AQ_realtime"></iframe>
+          <iframe id="ifrMap" height="600" width="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="מפת המפעלים המדווחים למפלס ממוינים לפי ענפי פעילות" src="http://beta.govmap.gov.il/map.aspx?c=eF08XGXO0ta3TQtFnHkc8w%3d%3d%7C5yAAYB898T4%2b6C1wazlNPA%3d%3d&amp;showBackBtn=1&amp;showNavBtn=1&amp;AllowDrag=1&amp;in=1&amp;height=650&amp;width=500&amp;z=0.5&amp;b=0&amp;mapMode=1&amp;lay=AQ_realtime"></iframe>
+        <div class="row span10" >
+          <div class="span2"> <img src="img/air-q-low.png" alt="air polution is low"> </div>
+          <div class="span2"> <img src="img/air-q-med.png" alt="air polution is med"> </div>
+          <div class="span2"> <img src="img/air-q-high.png" alt="air polution is high"> </div>
+          <div class="span2"> <img src="img/air-q-very-high.png" alt="air polution is very high"> </div>
+        </div>
       </div>
 
-        <div class="row">
+        <div class="row" dir="rtl">
           <h3 dir="rtl">מזג האויר</h3>
           <div class="span5" dir="rtl">
             <h4 dir="rtl">טמפרטורות</h4>
